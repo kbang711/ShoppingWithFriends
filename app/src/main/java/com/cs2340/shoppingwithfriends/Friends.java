bc7df3ae00309153bcea3d58aebc830e92f89b0b
Kevin Bang <bang3@gatech.edu>
2015-02-10 18:51:50 -0500

    Added a class for Friends and a Friends Page (Page will have option to add or view friends)

---
+++ app/src/main/java/com/cs2340/shoppingwithfriends/Friends.java
@@ -0,0 +1,45 @@
+package com.cs2340.shoppingwithfriends;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Class that does the main Friends page (has option to go to friends list or add friends)
+ * Created by Kevin Bang on 2/10/2015.
+ */
+public class Friends extends ActionBarActivity {
+    /**
+     * Called when activity is first created
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
