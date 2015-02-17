ebe226bf68a4ccc4eed95dcbd4c1b67afc98ef38
Kevin Bang <bang3@gatech.edu>
2015-02-17 17:44:55 -0500

    Adding to AddFriend

--- app/src/main/java/com/cs2340/shoppingwithfriends/AddFriend.java
+++ app/src/main/java/com/cs2340/shoppingwithfriends/AddFriend.java
@@ -4,14 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class AddFriend extends ActionBarActivity {
+    private EditText username = null;
+    private EditText email = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        username = (EditText)findViewById(R.id.);
+    }
+
+    public void addFriend(View view) {
+        if ()
     }
 
 
