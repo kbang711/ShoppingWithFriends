6916b85149072a3c049ead8ed4d6240198b7b689
Kevin Bang <bang3@gatech.edu>
2015-02-21 11:18:40 -0500

    Added button to go back to Friends from FriendDetail. Added button to remove Friend in Friend Detail. Issue: Need to figure out a way to remove friends from both people affected (currently only affects person removing the friend).

--- app/src/main/java/com/cs2340/shoppingwithfriends/FriendDetail.java
+++ app/src/main/java/com/cs2340/shoppingwithfriends/FriendDetail.java
@@ -1,10 +1,14 @@
 package com.cs2340.shoppingwithfriends;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Kevin Bang on 2/19/2015.
@@ -12,6 +16,7 @@ import android.widget.TextView;
 public class FriendDetail extends ActionBarActivity {
     private TextView textViewName, textViewEmail, textViewRating, textViewNumberOfSalesReports;
     Person friend;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,23 @@ public class FriendDetail extends ActionBarActivity {
         textViewEmail.setText(friend.getEmail());
         textViewRating.setText("");
         textViewNumberOfSalesReports.setText("0");
+
+        back = (Button)findViewById(R.id.buttonBack);
+    }
+
+    /**
+     * Goes back to the Friends Page
+     */
+    public void back(View view) {
+        startActivity(new Intent(getApplicationContext(), Friends.class));
+    }
+
+    public void removeFriend(View view) {
+        //Need to figure out a way to remove yourself from that "friend's" friend list
+        Person.friends.remove(Friends.personClicked);
+        startActivity(new Intent(getApplicationContext(), Friends.class));
+        Toast.makeText(getApplicationContext(), "Removed Friend",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
