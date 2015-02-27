56009f6f1dbaa45877dc4afb32f9a99813fc72df
Jack Li <jli@lawn-128-61-121-61.lawn.gatech.edu>
2015-02-27 13:27:22 -0500

    new file for adding bugs

--- app/src/main/java/com/cs2340/shoppingwithfriends/Friends.java
+++ app/src/main/java/com/cs2340/shoppingwithfriends/Friends.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
