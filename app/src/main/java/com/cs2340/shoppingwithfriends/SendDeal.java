bae4a533c68454d6c5436770ffa920048cbaa069
Kevin Bang <bang3@gatech.edu>
2015-03-12 10:27:15 -0400

    Added more to xml of Send Deal. No functionality

--- app/src/main/java/com/cs2340/shoppingwithfriends/SendDeal.java
+++ app/src/main/java/com/cs2340/shoppingwithfriends/SendDeal.java
@@ -1,9 +1,46 @@
 package com.cs2340.shoppingwithfriends;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Kevin Bang on 3/12/2015.
  */
 public class SendDeal extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_deal);
+    }
+
+    /**
+     * Goes back to the Friends Page
+     */
+    public void back(View view) {
+        startActivity(new Intent(getApplicationContext(), ItemList_Friend.class));
+    }
+
+    public void removeItem(View view) {
+        startActivity(new Intent(getApplicationContext(), SendDeal.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
