27ffeca01f74c98b8dbcab60998066657f6097ce
Abhilash <abhilashnair1994@gmail.com>
2015-04-19 16:28:24 -0400

    Extenstive Junit

--- app/src/main/java/com/cs2340/shoppingwithfriends/junits/junitTests.java
+++ app/src/main/java/com/cs2340/shoppingwithfriends/junits/junitTests.java
@@ -138,4 +138,77 @@ public class junitTests extends InstrumentationTestCase {
 
 
     }
+    public void testValidEmail() {
+        Registration email = new Registration();
+        ArrayList<Person> friends = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
+        Person Person1 = new Person("Jihad Iqbal","Iqbal.j@gatech.edu", "jam", "bam", friends, items);
+        Person Person2 = new Person("Abhi nair","anair35@gatech.edu", "jam", "bam", friends, items);
+        Person wrongPerson1 = new Person("James Franco","Iqbal.j@gatech", "jam", "bam", friends, items);
+        Person wrongPerson2 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+        String correctEmail = "abhilashnair1994@gmail.com";
+        String wrongEmail = "blah@gamcom";
+
+        assertTrue("Should be correct email format", correctEmail.matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+        assertFalse("Should be wrong email format ", wrongEmail.matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+
+        assertTrue("Should be correct email format", Person1.getEmail().matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+        assertTrue("Should be correct email format", Person2.getEmail().matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+        assertFalse("Should be wrong email format", wrongPerson1.getEmail().matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+        assertFalse("Should be wrong email format", wrongPerson2.getEmail().matches("[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"));
+
+    }
+
+    public void testPassword() {
+        Registration password = new Registration();
+        ArrayList<Person> friends = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
+        Person Person1 = new Person("Jihad Iqbal","Iqbal.j@gatech.edu", "jamsdfkasdfs", "bam", friends, items);
+        Person Person2 = new Person("Abhi nair","anair35@gatech.edu", "jam", "bam", friends, items);
+        Person Person3 = new Person("James Franco","Iqbal.j@gatech", "123", "bam", friends, items);
+        Person Person4 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+        Person Person5 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+
+        assertTrue("Should be correct password format", Person1.getPassword().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person2.getPassword().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person3.getPassword().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person4.getPassword().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person5.getPassword().matches("[0-9A-Za-z](\\w|\\S)*$"));
+
+    }
+
+    public void testUsername() {
+        Registration username = new Registration();
+        ArrayList<Person> friends = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
+        Person Person1 = new Person("Jihad Iqbal","Iqbal.j@gatech.edu", "jamsdfkasdfs", "bam", friends, items);
+        Person Person2 = new Person("Abhi nair","anair35@gatech.edu", "jam", "bam", friends, items);
+        Person Person3 = new Person("James Franco","Iqbal.j@gatech", "123", "bam", friends, items);
+        Person Person4 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+        Person Person5 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+
+        assertTrue("Should be correct password format", Person1.getUsername().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person2.getUsername().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person3.getUsername().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person4.getUsername().matches("[0-9A-Za-z](\\w|\\S)*$"));
+        assertTrue("Should be correct password format ", Person5.getUsername().matches("[0-9A-Za-z](\\w|\\S)*$"));
+    }
+
+    public void testName() {
+        Registration email = new Registration();
+        ArrayList<Person> friends = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
+        Person Person1 = new Person("Jihad Iqbal","Iqbal.j@gatech.edu", "jamsdfkasdfs", "bam", friends, items);
+        Person Person2 = new Person("Abhi nair","anair35@gatech.edu", "jam", "bam", friends, items);
+        Person Person3 = new Person("James Franco","Iqbal.j@gatech", "123", "bam", friends, items);
+        Person Person4 = new Person("Dave Franko","Iqbalgatech.edu", "jam", "bam", friends, items);
+        Person Person5 = new Person("123213 232132","Iqbalgatech.edu", "jam", "bam", friends, items);
+
+        assertTrue("Should be correct name format", Person1.getName().matches("[a-zA-z]+([ '-][a-zA-Z]+)*"));
+        assertTrue("Should be correct name format", Person2.getName().matches("[a-zA-z]+([ '-][a-zA-Z]+)*"));
+        assertTrue("Should be correct name format", Person3.getName().matches("[a-zA-z]+([ '-][a-zA-Z]+)*"));
+        assertTrue("Should be correct name format", Person4.getName().matches("[a-zA-z]+([ '-][a-zA-Z]+)*"));
+        assertFalse("Should be correct name format", Person5.getName().matches("[a-zA-z]+([ '-][a-zA-Z]+)*"));
+    }
+
 }
